89ada2971d29ee476e1140ea97b101aad75d56aa
xipok <lundydr@gmail.com>
2015-11-05 22:15:37 -0500

    changed first test to report errors, added second test for too late end, added test for 2/hr rate rule only

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -1,5 +1,11 @@
 class Babysitter
 	def calc(start_time, bed_time, end_time)
-		nil
+		if start_time < 17
+			"cannot start before 5:00PM(1700)"
+		elsif end_time > 4 && end_time < 17
+			"cannot end after 4:00AM(0400)"
+		else
+			(end_time - start_time)*12
+		end
 	end
 end
--- spec/babysitter_spec.rb
+++ spec/babysitter_spec.rb
@@ -1,10 +1,22 @@
 require "babysitter"
 
 describe Babysitter do
+
 	describe '#calc' do
-		it 'returns nil if the start time is before 5:00PM' do
-			babysitter = Babysitter.new
-			expect(babysitter.calc(4,5,6)).to eq(nil)		
+		before(:each) do
+			@babysitter = Babysitter.new
+		end
+
+		it 'returns an error if the start time is before 5:00PM(17:00)' do
+			expect(@babysitter.calc(16,18,19)).to eq('cannot start before 5:00PM(1700)')		
+		end
+
+		it 'returns an error if the end time is after 4:00AM' do
+			expect(@babysitter.calc(17,18,5)).to eq('cannot end after 4:00AM(0400)')
+		end
+
+		it 'returns a total of 48 dollars for a shift from 6:00PM to 10:00PM, with a bedtime of 10:00PM' do
+			expect(@babysitter.calc(18,22,22)).to eq(48)
 		end
 	end
 end
